00054c3dde9fee97147fc69934928f6bfd9c6091
A. Dwi Rekno Pangestu <adwireknopangestu@gmail.com>
2025-10-05 08:32:25 +0700

    Init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//deklarasi variabel
+
+float sisi,luas;
+
+cout<< "menghirung luas persegi" << endl;
+cout<< "masukan sis panjang persegi :";
+cin>> sisi;
+
+luas = sisi*sisi;
+
+
+cout<< "luas persegi adalah:"<< luas<< endl;
+
+}
